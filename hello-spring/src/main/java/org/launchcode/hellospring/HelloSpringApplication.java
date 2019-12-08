59b924bf29e7ef0ef30afc07bd133bd0d2265b7d
varbogas <55903701+varbogas@users.noreply.github.com>
2019-12-08 15:51:37 -0600

    Finished example.

--- hello-spring/src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
@@ -2,9 +2,6 @@ package org.launchcode.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class HelloSpringApplication {
@@ -12,13 +9,5 @@ public class HelloSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
-
-	@RequestMapping(value = "/form", method = {RequestMethod.GET, RequestMethod.POST})
-	public String helloWithQueryParam(@RequestParam String name, String language) {
-
-		return getMessage(name, language);
-	}
-
-
 
 }
--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,70 +1,41 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("/")
 public class HelloController {
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-
-//    //lives at hello/goodbye
-//    @GetMapping("/goodbye")
-//    public String goodbye() {
-//        return "Goodbye, Spring!";
-//    }
-
-    @RequestMapping(value = "/form/result", method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name, String language) {
-
-        return getMessage(name, language);
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
-    //lives at hello/name
-//    @GetMapping("/form/{name}")
-//    public String helloWithPathParam(@PathVariable String name){
-//
-//        return "Hello, " + name + "!";
-//    }
-
-
-    //lives at hello/form
-    @GetMapping("/")
-    public String helloForm(){
-               return  "<html>" +
-                        "<body>" +
-                        "<form action = '/form' method = 'post'>" +
-                        "<input type = 'text' name = 'name' />" +
-                        "<select name='language'>" +
-                        "<option value='English'>English</option>"  +
-                        "<option value='French'>French</option>"  +
-                        "<option value='Italian'>Italian</option>"  +
-                        "<option value='Japanese'>Japanese</option>"  +
-                        "<option value='Spanish'>Spanish</option>"  +
-                        "</select>" +
-                        "<input type = 'submit' value = 'Greet Me!' />" +
-                        "</form>" +
-                        "</body>" + "</html>";
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
-    public static String getMessage(String name, String language) {
-        if (language.equals("English")) {
-            return "Hello, " + name + "!";
-        } else if (language.equals("French")) {
-            return "Bonjour, " + name + "!";
-        } else if (language.equals("Italian")) {
-            return "Ciao, " + name + "!";
-        } else if (language.equals("Japanese")) {
-            return "Konbanwa, " + name + "!";
-        } else {
-            return "Hola, " + name + "!";
-        }
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("Javascript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
-
